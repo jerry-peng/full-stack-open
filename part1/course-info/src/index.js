3d786912920931b98753246bc3c8023532d32a39
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-09-25 22:10:07 -0700

    Refactored App components into Content and Part

--- part1/course-info/src/index.js
+++ part1/course-info/src/index.js
@@ -1,28 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exerCount}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.names[0]} exerCount={props.exerCounts[0]} />
+      <Part name={props.names[1]} exerCount={props.exerCounts[1]} />
+      <Part name={props.names[2]} exerCount={props.exerCounts[2]} />
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const names = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
+  const exerCounts = [10, 7, 14]
 
   return (
     <div>
       <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content names={names} exerCounts={exerCounts} />
+      <p>Number of exercises {exerCounts.reduce((acc, val) => acc + val)}</p>
     </div>
   )
 }
