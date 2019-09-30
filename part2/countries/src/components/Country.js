5deeac50ee792fc95976fb864601a4754abb760e
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-09-29 21:35:35 -0700

    Added weather api calls for Country components

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,12 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+
 const Country = ({country}) => {
+  const weatherstack_key = process.env.REACT_APP_WEATHERSTACK_API_KEY
   const flagAltText = `${country.name} flag`
+  const weatherIconAltText = `${country.capital} weather`
+	const [weather, setWeather] = useState({})
 
   const fetchWeather = () => {
-    axios.get(('http://api.weatherstack.com/current') => {
-    })
+    axios.get('http://api.weatherstack.com/current', {
+        params: {
+          access_key: weatherstack_key,
+          query: country.capital
+        } 
+      })
+      .then((res) => {
+				setWeather(res.data)
+      })
   }
 
   useEffect(fetchWeather, [])
@@ -24,6 +35,14 @@ const Country = ({country}) => {
             })}
           </ul>
           <img className="flags" src={country.flag} alt={flagAltText}/>
+          {Object.entries(weather).length !== 0&&
+            <>
+              <h2>Weather in {country.capital}</h2>
+              <p><b>Temperature: </b>{weather.current.temperature} Celsius</p>
+              <img src={weather.current.weather_icons[0]} alt={weatherIconAltText} />
+              <p><b>wind: </b>{weather.current.wind_speed} kph direction {weather.current.wind_dir}</p>
+            </>
+          }
         </>
       }
     </div>
