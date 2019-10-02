f8dc4ebe624f26ccf0542854a8409ff5421ee971
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-10-02 09:49:03 -0700

    uibuild

---
+++ part2/phonebook/build/precache-manifest.0442a16541d842d8275c75a4c182e5ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "940eb37bee4e50ccfd9613387084dcfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b95e94ba53c6170a2d84",
+    "url": "/static/css/main.b534fb66.chunk.css"
+  },
+  {
+    "revision": "85cdf5ff7efea6724410",
+    "url": "/static/js/2.3379b680.chunk.js"
+  },
+  {
+    "revision": "b95e94ba53c6170a2d84",
+    "url": "/static/js/main.6e6e74ea.chunk.js"
+  },
+  {
+    "revision": "6d5291d2a5802e186516",
+    "url": "/static/js/runtime-main.0101dce3.js"
+  }
+]);
