49d911fce3e2b3e6f47df075463426aa0ca4e908
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-10-01 21:07:38 -0700

    Added cors middleware, changed post to return newly added person object, changed port to use environment port first

--- part2/phonebook-backend/index.js
+++ part2/phonebook-backend/index.js
@@ -19,6 +19,9 @@ const morganConfig = (tokens, req, res) => {
 }
 app.use(morgan(morganConfig))
 
+const cors = require('cors')
+app.use(cors())
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
@@ -78,7 +81,7 @@ app.post('/api/persons', (req, res) => {
   }
 
   persons = persons.concat(person)
-  res.json(persons)
+  res.json(persons[persons.length - 1])
 })
 
 app.get('/api/persons', (req, res) => {
@@ -109,7 +112,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
