868805b44fd63c7c7cdd690fdb2495c4e6877c71
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-10-01 23:09:20 -0700

    revert static file path change

--- part2/phonebook/index.js
+++ part2/phonebook/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-app.use(express.static(__dirname + 'build'))
+app.use(express.static('build'))
 
 const morgan = require('morgan')
 const morganConfig = (tokens, req, res) => {
