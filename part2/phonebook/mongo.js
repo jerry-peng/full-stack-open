4a2d85d517c69a2f01378b68b50abc8e59aedbca
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-10-05 13:01:32 -0700

    Test mongoose file that interact with mongodb

---
+++ part2/phonebook/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://admin-user:${password}@mycluster-eibbp.mongodb.net/test?retryWrites=true&w=majority`
+
+const config = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+
+mongoose.connect(url, config)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(person.name, person.number) 
+    }) 
+    mongoose.connection.close()
+  })
+}
+else {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+
+  person.save().then(res => {
+    console.log(`added ${res.name} ${res.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
