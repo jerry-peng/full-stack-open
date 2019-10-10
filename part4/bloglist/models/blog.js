6384070282b61daee1e654ca016ec2b13d1c6229
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-10-09 19:04:55 -0700

    Added fixes to refactoring bugs

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,18 +1,4 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
-
-const mongoUrl = config.MONGODB_URI
-
-mongoose
-  .connect(mongoUrl, {
-    useNewUrlParse: true
-  })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message) 
-  })
 
 const blogSchema = mongoose.Schema({
   title: String,
--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 
-module.export = {
+module.exports = {
   MONGODB_URI,
   PORT
 }
