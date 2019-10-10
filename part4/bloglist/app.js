692954289ac59ce58021bc7f53c51d637ef4439e
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-10-09 19:04:39 -0700

    Added app.js after refactor

---
+++ part4/bloglist/app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParse: true
+  })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message) 
+  })
+
+const app = express()
+app.use(cors())
+app.use(express.static('build'))
+app.use(bodyParser.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
