68f912b195f1797f0ea5963f41803b9927a49e04
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-10-22 21:24:18 -0700

    Added usage of usersRouter in app

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -31,6 +32,7 @@ app.use(bodyParser.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
