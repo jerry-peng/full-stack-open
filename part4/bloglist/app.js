7dbf3078e9582196022a816e660201f188cd3e9a
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-10-25 11:52:20 -0700

    Add more options to mongoose create function to remove deprecation warnings

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-
 
 if (process.env.NODE_ENV !== 'test') {
   logger.info('connecting to', config.MONGODB_URI)
@@ -16,6 +16,8 @@ if (process.env.NODE_ENV !== 'test') {
     .connect(config.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology:true
+      useFindAndModify: false,
+      useCreateIndex: true
     })
     .then(() => {
       logger.info('connected to MongoDB')
@@ -33,6 +35,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
