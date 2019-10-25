55b7dcc26e659067fafbff952e720d22867aa585
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-10-25 11:53:25 -0700

    Add SECRET envvar

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,9 +1,12 @@
-require('dotenv').config()
+const envPath = __dirname + "/../.env"
+require('dotenv').config({ path: envPath })
 
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
+let SECRET = process.env.SECRET
 
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  SECRET
 }
