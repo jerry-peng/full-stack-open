55d4bb31ac348dcd33362fdc756295ba5c8627c7
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-10-25 22:10:19 -0700

    Automatic eslint fix

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -18,7 +18,7 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'JsonWebTokenError') {
-		return response.status(401).json({ error: 'invalid token' })
+    return response.status(401).json({ error: 'invalid token' })
   }
 
   logger.error(error.message)
